5f934a77972bdcfee860fcdb14bfcfa7c8620f02
Rodion Sviridov <sviridov.rod@yandex.ru>
2023-09-12 15:58:40 +0300

    Practice 2_4 backend fixes

--- practice2_4rsocket/rsocket_backend/src/main/kotlin/com/example/Application.kt
+++ practice2_4rsocket/rsocket_backend/src/main/kotlin/com/example/Application.kt
@@ -10,7 +10,7 @@ import io.ktor.server.websocket.*
 import io.rsocket.kotlin.ktor.server.RSocketSupport
 
 fun main() {
-	embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+	embeddedServer(Netty, port = 8008, host = "0.0.0.0", module = Application::module)
 		.start(wait = true)
 }
 
--- practice2_4rsocket/rsocket_backend/src/main/kotlin/com/example/database/Database.kt
+++ practice2_4rsocket/rsocket_backend/src/main/kotlin/com/example/database/Database.kt
@@ -1,9 +1,11 @@
 package com.example.database
 
+import com.example.model.Article
 import com.example.model.Articles
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -14,6 +16,20 @@ object DatabaseFactory {
 		val database = Database.connect(jdbcURL, driverClassName)
 		transaction(database) {
 			SchemaUtils.create(Articles)
+		}
+		listOf<Article>(
+			Article(id = 1, title = "Title of the article", body = "The body of the article"),
+			Article(id = 2, title = "Another title of the article", body = "More content of the article"),
+			Article(id = 3, title = "A third title of the article", body = "Yet more content of the article"),
+			Article(id = 4, title = "A fourth title of the article", body = "Last part of the article content"),
+			Article(id = 5, title = "And a fifth title of the article", body = "Final content of article")
+		).forEach { article ->
+			transaction {
+				Articles.insert {
+					it[Articles.title] = article.title
+					it[Articles.body] = article.body
+				}
+			}
 		}
 	}
 
